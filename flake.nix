fc80223d16c51a29e1883aab8777afd3c0353051
James Ottaway <git@james.b12y.com>
2020-11-21 17:18:22 +1000

    Nixify

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  description = "ulid";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+
+  outputs = { self, nixpkgs }: {
+    defaultPackage.x86_64-darwin = nixpkgs.legacyPackages.x86_64-darwin.callPackage ./. {};
+  };
+}
